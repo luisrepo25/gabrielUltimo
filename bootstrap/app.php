c4acba9c38ab327cc7a43747d8a423cf2a4f9338
Luis F Blanco <blanco.luis@ficct.uagrm.edu.bo>
2026-07-05 20:59:50 -0400

    feat: configuracion de infraestructura para Vercel Serverless

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,3 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$app->useStoragePath(isset($_ENV['VERCEL']) ? '/tmp/storage' : storage_path());
+
+return $app;
